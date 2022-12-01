cb2d12c5f5c68bd3b97573f7418eec4d885655a3
jgnacio <jgnaciogomez@gmail.com>
2022-12-01 06:29:35 -0800

    concat string and make a va_list for every single str

--- path_concat.c
+++ path_concat.c
@@ -8,27 +8,47 @@
  */
 char *path_concat(int cnt_str, char *s1, ...)
 {
-	int len_sn = 0, len_sn1 = 0, i = 0, j = 0;
+	int len_sn = 0, i = 0, j = 0;
 	char *concat_buf = NULL;
 	va_list c_str;
-	list_t str_list;
+	list_t *str_list = NULL, *tmp = NULL;
 
 	va_start(c_str, s1);
 	
-	while (cnt_str)
+	
+	while (cnt_str >= 0)
 	{
 		if (s1 == NULL)
 		{
 			s1 = "";
 		}
+		tmp = add_node_end(&str_list, s1);
+		len_sn += tmp->len;
+
 		printf("%s\n", s1);
 		s1 = va_arg(c_str, char *);
 		cnt_str--;
 	}
 
-	/*
-	concat_buf = malloc((len_s1 + len_s2) + 1);
+	printf("len_FULL = %d\n", len_sn);
 
+	concat_buf = malloc(len_sn + 1);
+
+	if (str_list)
+	{
+		while (str_list)
+		{
+			printf("existe\n");
+			s1 = str_list->str;
+			for (i = 0; s1[i]; i++, j++)
+				concat_buf[j] = s1[i];
+			printf("strconcat= '%s'\n", concat_buf);
+			str_list = str_list->next;
+		}
+	}
+	printf("no existe\n");
+
+	/*
 	if (concat_buf)
 	{
 		for (i = 0; i <= (len_s1 + len_s2); i++)
